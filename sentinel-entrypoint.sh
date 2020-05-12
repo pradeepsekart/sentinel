8b5b0814f145697477c2af0cad6aed5c09db07e7
Pradeep Kumar S <pradeepsekart@gmail.com>
2020-05-12 08:30:06 +0530

    Changed IP

--- sentinel-entrypoint.sh
+++ sentinel-entrypoint.sh
@@ -4,5 +4,6 @@ sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /redis/sentinel.conf
 sed -i "s/\$REDIS_PASSWORD/$REDIS_PASSWORD/g" /redis/sentinel.conf
+sed -i "s/\$REDIS_PUBLIC_IP/$REDIS_PUBLIC_IP/g" /redis/sentinel.conf
 
 redis-server /redis/sentinel.conf --sentinel
